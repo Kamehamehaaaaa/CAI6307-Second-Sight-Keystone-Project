33484e65a27343282bccf7a2b751de091f07e554
CWDE615 <cwellis001@gmail.com>
2025-03-13 16:17:47 -0400

    Add more boilerplate to run_eval.py and run_model.py

--- run_eval.py
+++ run_eval.py
@@ -1,2 +1,13 @@
 # CWDE615 3/13/25
 # Run evaluation metrics on the data in output.
+
+def kldiv(data):
+	pass
+
+def cos_sim(data):
+	pass
+
+
+if __name__ == "__main__":
+	# TODO: create arg structure for running functions that calculate evaluation metrics (KL Div, Cos Sim.)
+
--- run_model.py
+++ run_model.py
@@ -2,6 +2,15 @@
 # Script that runs the model on the data in the provided directories.
 import os
 
+def modelA(data):
+	# TODO: In each model function, set up the model using the script provided by its maintainers and call it likewise on the data.
+	pass
+
+def model_router(arg = None, data = None):
+	if arg = None:
+		print("No arg provided to router.")
+	else:
+		print("Invalid arg provided to router.")
 
 if __name__ == "__main__":
-	# TODO: Use args to call the right model function.
+	# TODO: Use args to call the right model_router function, which in turn, calls the various other model functions on the same data.
